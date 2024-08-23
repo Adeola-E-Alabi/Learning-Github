d4dc729ec97d93a1c451101b951d08bd005e9e55
Adeola-Alabi <Erron.smith2020@gmail.com>
2024-08-22 22:15:57 -0700

    Working in the create-function branch

--- Python code.py
+++ Python code.py
@@ -1,16 +1,17 @@
-import random
-x = "The letter X"
-capitalize = False
-array = []
-rand_int = random.choice(range(1,21))
-while rand_int > 0:
-    array.append(rand_int)
-    rand_int -= 1
+def toggle_capitalize():
+    import random
+    x = "The letter X"
+    capitalize = False
+    array = []
+    rand_int = random.choice(range(1,21))
+    while rand_int > 0:
+        array.append(rand_int)
+        rand_int -= 1
 
-for x in array:
-    capitalize = (not capitalize)
-    if capitalize == True:
-        x = "THE LETTER X"
-    else:
-        x = "the letter x"
-print(x)
+    for x in array:
+        capitalize = (not capitalize)
+        if capitalize == True:
+            x = "THE LETTER X"
+        else:
+            x = "the letter x"
+    print(x)
